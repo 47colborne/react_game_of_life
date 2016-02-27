65561b1f3d875dc3ffea93134f129cdadd6ca98f
Alishan Ladhani <Alishan.Ladhani@gmail.com>
2016-02-26 23:53:13 -0500

    Notify neighbour cells of inital seed

--- app/assets/javascripts/components/Cell.jsx
+++ app/assets/javascripts/components/Cell.jsx
@@ -1,6 +1,14 @@
 const Cell = React.createClass({
+  getInitialState() {
+    return {neighbourCount: 0}
+  },
+
   getDefaultProps() {
-    return {alive: false, neighbourCount: 0}
+    return {alive: false}
+  },
+
+  modifyNeighbourCount(num) {
+    this.setState({neighbourCount: this.state.neighbourCount + num})
   },
 
   render() {
--- app/assets/javascripts/components/Game.jsx
+++ app/assets/javascripts/components/Game.jsx
@@ -17,13 +17,65 @@ const Game = React.createClass({
     this.seedCells(cells)
   },
 
+  componentDidMount() {
+    this.seeds.forEach((index) => {
+      this.notifyNeighbouringCells(index);
+    })
+  },
+
   seedCells(cells) {
+    this.seeds = [];
     for (var i = 0; i < this.props.livingCells; i++) {
       let rand = Math.floor(Math.random() * this.props.numCells);
       console.log(`Making cell #${rand} alive`);
       cells[rand] = <Cell alive={true} ref={(cell) => this.cells.push(cell)} />;
-      this.setState({cells: cells})
+      this.seeds.push(rand);
     }
+    this.setState({cells: cells});
+    this.deduplicateSeeds();
+  },
+
+  deduplicateSeeds() {
+    function onlyUnique(value, index, self) {
+      return self.indexOf(value) === index;
+    }
+    this.seeds = this.seeds.filter(onlyUnique)
+  },
+
+  notifyNeighbouringCells(index) {
+    let getNeighbouringIndices = (index) => {
+      let neighbourIndices = [];
+
+      // Left unless at beginning of row
+      if (index % this.props.rowLength != 0) {
+        neighbourIndices.push(index - 1);
+      }
+
+      // Right unless at end of row
+      if (index % this.props.rowLength != (this.props.rowLength - 1)) {
+        neighbourIndices.push(index + 1);
+      }
+
+      // Up + diagonal
+      neighbourIndices.push(index - this.props.rowLength + 1);
+      neighbourIndices.push(index - this.props.rowLength);
+      neighbourIndices.push(index - this.props.rowLength - 1);
+
+      // Down + diagonal
+      neighbourIndices.push(index + this.props.rowLength + 1);
+      neighbourIndices.push(index + this.props.rowLength);
+      neighbourIndices.push(index + this.props.rowLength - 1);
+
+      return neighbourIndices
+    };
+
+    let _this = this;
+
+    getNeighbouringIndices(index).forEach((index) => {
+      if (_this.cells[index]) {
+        _this.cells[index].modifyNeighbourCount(1)
+      }
+    })
   },
 
   endOfRow(num) {
