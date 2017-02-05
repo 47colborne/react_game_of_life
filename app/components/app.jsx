2549ebfba0489e0bbd84404814cc4f111c7c1df5
Alishan Ladhani <Alishan.Ladhani@gmail.com>
2017-02-05 14:22:52 -0500

    Migrate to brunch

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -2,14 +2,18 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Game from './Game'
 
-import '../styles/app';
-
 class App extends Component {
-	render () {
-		return (
-			<Game />
-		)
-	}
+  render () {
+    return (
+      <div>
+        <h1>Conway's Game of Life</h1>
+        <a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life" rel="nofollow">
+          What is it?
+        </a>
+        <Game />
+      </div>
+    )
+  }
 }
 
 export default App;
